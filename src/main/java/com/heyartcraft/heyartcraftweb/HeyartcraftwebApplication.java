704ad5ffd910547f66a1559176b2f7c3c7a3aa62
MartiRoll29 <141012622+MartiRoll29@users.noreply.github.com>
2026-07-01 02:41:24 -0600

    Agrega estructura inicial del proyecto Spring Boot (Hey Artcraft Web)

---
+++ src/main/java/com/heyartcraft/heyartcraftweb/HeyartcraftwebApplication.java
@@ -0,0 +1,13 @@
+package com.heyartcraft.heyartcraftweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeyartcraftwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeyartcraftwebApplication.class, args);
+	}
+
+}
