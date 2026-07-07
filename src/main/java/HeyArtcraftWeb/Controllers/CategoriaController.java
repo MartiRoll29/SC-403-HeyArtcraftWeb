6e7b1e9a3caa890afc0f3313188443ff8705eb43
Aryeri Torres <yeye120625@gmail.com>
2026-07-06 18:09:56 -0600

    3

--- src/main/java/HeyArtcraftWeb/Controllers/CategoriaController.java
+++ src/main/java/HeyArtcraftWeb/Controllers/CategoriaController.java
@@ -27,28 +27,28 @@ public class CategoriaController {
         var categorias = categoriaService.getCategoriasConProductos();
         model.addAttribute("categorias", categorias);
         model.addAttribute("categoria", new Categoria());
-        return "categoria/listado";
+        return "catalogo/listado";
     }
 
     @PostMapping("/guardar")
     public String guardar(@ModelAttribute Categoria categoria) {
         categoriaService.save(categoria);
-        return "redirect:/categoria/listado";
+        return "redirect:/catalogo/listado";
     }
     
     @PostMapping("/eliminar")
     public String eliminar(@RequestParam("id") Integer id) {
         categoriaService.delete(id);
-        return "redirect:/categoria/listado";
+        return "redirect:/catalogo/listado";
     }
 
     @GetMapping("/modificar/{id}")
     public String modificar(@PathVariable("id") Integer id, Model model) {
         Optional<Categoria> categoriaOpt = categoriaService.getCategoria(id);
         if (categoriaOpt.isEmpty()) {
-            return "redirect:/categoria/listado";
+            return "redirect:/catalogo/listado";
         }
         model.addAttribute("categoria", categoriaOpt.get());
-        return "categoria/modifica";
+        return "catalogo/modifica";
     }
 }
